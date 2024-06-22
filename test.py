c8e74fcecee57ce354cb3cb17bb0b376fb5f771e
Fabian Roscher <roscher2003@outlook.de>
2024-06-22 11:25:43 +0200

    fixed test cases

--- test.py
+++ test.py
@@ -1,28 +1,31 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, call
 import main
 
 class TestMainFunction(unittest.TestCase):
     def setUp(self):
-        self.mock_flatpak_list = "Ref\tName\tBranch\tArch\tInstalledSize\tOrigin\tInstallation\ncom.spotify.Client\tSpotify\tstable\tx86_64\t1.2 GB\tsystem\n"
+        self.mock_flatpak_list = "LibreOffice\torg.libreoffice.LibreOffice\t24.2.4.2\tstable\tflathub\tsystem\n"
 
+    @patch('main.create_alias')
     @patch('subprocess.run')
-    def test_single_app_alias_creation(self, mock_run):
+    def test_single_app_alias_creation(self, mock_run, mock_create_alias):
         mock_run.return_value.stdout = self.mock_flatpak_list
         main.main()
-        mock_run.assert_called_with(['bash', '-c', 'alias spotify="flatpak run com.spotify.Client"'], check=True)
+        mock_create_alias.assert_called_once_with('libreoffice', 'org.libreoffice.LibreOffice')
 
+    @patch('main.create_alias')
     @patch('subprocess.run')
-    def test_special_cases_alias_creation(self, mock_run):
-        mock_run.return_value.stdout = "Ref\tName\tBranch\tArch\tInstalledSize\tOrigin\tInstallation\ncom.bitwarden.desktop\tBitwarden\tstable\tx86_64\t1.2 GB\tsystem\n"
+    def test_special_cases_alias_creation(self, mock_run, mock_create_alias):
+        mock_run.return_value.stdout = "Bitwarden\tcom.bitwarden.desktop\t2024.6.1\tstable\tflathub\tsystem\n"
         main.main()
-        mock_run.assert_called_with(['bash', '-c', 'alias bw="flatpak run com.bitwarden.desktop"'], check=True)
+        mock_create_alias.assert_called_once_with('bw', 'com.bitwarden.desktop')
 
+    @patch('main.create_alias')
     @patch('subprocess.run')
-    def test_no_apps_installed(self, mock_run):
-        mock_run.return_value.stdout = "Ref\tName\tBranch\tArch\tInstalledSize\tOrigin\tInstallation\n"
+    def test_no_apps_installed(self, mock_run, mock_create_alias):
+        mock_run.return_value.stdout = ""
         main.main()
-        mock_run.assert_not_called()
+        mock_create_alias.assert_not_called()
 
 if __name__ == '__main__':
     unittest.main()
